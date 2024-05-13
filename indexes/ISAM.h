b2e4ac34bcd12ea2743d290d876075009e7215f4
leonardocandio <leonardo.candio@utec.edu.pe>
2024-05-13 18:40:31 -0500

    build ISAM partially finished
    logic for builing indexes is finished but wasnt able to try it out
    because we haven't processed the dataset into binary

--- indexes/ISAM.h
+++ indexes/ISAM.h
@@ -55,23 +55,20 @@ static constexpr size_t M = (PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) /
 template <typename KeyType> struct IndexPage {
     KeyType key[M<KeyType>];           // Array of keys
     size_t children[M<KeyType> + 1]{}; // Pointers to children
-    size_t n = 0; // Counts the number of keys in the index page.
+    size_t n_keys = 0; // Counts the number of keys in the index page.
 
     char *c_str() { return reinterpret_cast<char *>(this); }
 };
 
 /////////// Data Page ////////
 
-template <typename DataKeyType, typename DataAddressType>
-static constexpr size_t N = (PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) /
-                            sizeof(pair<DataKeyType, DataAddressType>);
-
-template <typename DataKeyType, typename DataAddressType> struct DataPage {
-    pair<DataKeyType, DataAddressType>
-        records[N<DataKeyType, DataAddressType>]; // Array of pair <key:address>
-    size_t next = -1;                             // Pointer to overflow page
-    size_t n_records = 0;                         // Number of records
-    //
+template <typename RecordType>
+static constexpr size_t N =
+    (PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) / sizeof(RecordType);
+template <typename RecordType> struct DataPage {
+    RecordType records[N<RecordType>]; // Array of records
+    size_t next = -1;                  // Pointer to overflow page
+    size_t n_records = 0;              // Number of records
 
     char *c_str() { return reinterpret_cast<char *>(this); }
 };
@@ -81,9 +78,10 @@ template <typename RecordType> class ISAM {
     ISAM(filesystem::path _data_path) : data_path(_data_path) { initialize(); }
 
   private:
-    filesystem::path data_path;
-    unordered_map<int, filesystem::path> index_paths;
-    size_t data_size;
+    filesystem::path data_path; // Path to the data file
+    unordered_map<int, filesystem::path>
+        index_paths;  // Paths to the index files
+    size_t data_size; // Size of the data file in bytes
 
     void initialize() {
 
@@ -110,21 +108,17 @@ template <typename RecordType> class ISAM {
         }
         data_file.close();
 
-        if (all_indexes_exist()) {
-            restore();
-        } else {
-            build(false);
+        if (!all_indexes_exist()) {
+            build(true);
         }
     }
 
-    void restore() {}
-
     // Building ISAM indexes bottom-up
-    void build(bool drop) {
+    void build(bool drop_existing) {
 
-        if (drop) {
+        if (drop_existing) {
             for (int i = 0; i < 3; i++) {
-                remove(index_paths[i]);
+                filesystem::remove(index_paths[i]);
             }
         }
 
@@ -136,28 +130,67 @@ template <typename RecordType> class ISAM {
     void build_upper_levels() {
         fstream data_index_file(index_paths[2], ios::in | ios::binary);
         fstream second_index_file(index_paths[1], ios::out | ios::binary);
-        DataPage<typename RecordType::id, size_t> data_index_page;
+        DataPage<RecordType> data_index_page;
         IndexPage<typename RecordType::id> second_index_page;
 
         size_t data_index_page_address = 0;
+        // Build second index page from the data index page
+        while (data_index_file.read(reinterpret_cast<char *>(data_index_page),
+                                    sizeof(data_index_page))) {
 
-        // TODO: Build de second index page
-
-        while (second_index_page.n < M<typename RecordType::id>) {
-            data_index_file.read(data_index_page.c_str(),
-                                 sizeof(data_index_page));
-            second_index_page.key[second_index_page.n] =
-                data_index_page.records[0].first;
-            second_index_page.children[second_index_page.n] =
-                data_index_page_address;
-            second_index_page.n++;
-            if (data_index_page.next == -1) {
-                break;
+            if (second_index_page.n_keys == M<typename RecordType::id>) {
+                second_index_page.children[second_index_page.n_keys] =
+                    data_index_page_address;
+                second_index_file.write(second_index_page.c_str(),
+                                        sizeof(second_index_page));
+                second_index_page.n_keys = 0;
             }
-            data_index_page_address = data_index_file.tellg();
+            second_index_page.children[second_index_page.n_keys] =
+                data_index_page_address;
+            second_index_page.key[second_index_page.n_keys] =
+                data_index_page.records[0].id;
+            second_index_page.n_keys++;
         }
 
-        // TODO:  build the first index page
+        if (second_index_page.n_keys > 0) {
+            second_index_page.children[second_index_page.n_keys] =
+                data_index_page_address;
+            second_index_file.write(second_index_page.c_str(),
+                                    sizeof(second_index_page));
+        }
+
+        data_index_file.close();
+
+        // Builds uppermost index page (root) from the second index page
+        fstream first_index_file(index_paths[0], ios::out | ios::binary);
+        IndexPage<typename RecordType::id> first_index_page;
+        size_t second_index_page_address = 0;
+        while (
+            second_index_file.read(reinterpret_cast<char *>(second_index_page),
+                                   sizeof(second_index_page))) {
+            if (first_index_page.n_keys == M<typename RecordType::id>) {
+                first_index_page.children[first_index_page.n_keys] =
+                    second_index_page_address;
+                first_index_file.write(first_index_page.c_str(),
+                                       sizeof(first_index_page));
+                first_index_page.n_keys = 0;
+            }
+            first_index_page.children[first_index_page.n_keys] =
+                second_index_page_address;
+            first_index_page.key[first_index_page.n_keys] =
+                second_index_page.key[0];
+            first_index_page.n_keys++;
+        }
+
+        if (first_index_page.n_keys > 0) {
+            first_index_page.children[first_index_page.n_keys] =
+                second_index_page_address;
+            first_index_file.write(first_index_page.c_str(),
+                                   sizeof(first_index_page));
+        }
+
+        second_index_file.close();
+        first_index_file.close();
     }
 
     // Build the bottom level (leaves) of the ISAM index (data pages)
@@ -165,29 +198,23 @@ template <typename RecordType> class ISAM {
         fstream data_file(data_path, ios::in | ios::binary);
         fstream index_file(index_paths[2], ios::out | ios::binary);
 
-        DataPage<typename RecordType::id, size_t> data_page;
+        DataPage<RecordType> data_page;
+        RecordType record;
 
-        while (data_file.tellg() < data_size) {
-
-            while (data_page.n_records < N<size_t, size_t>) {
-                typename RecordType::id read_id;
-                typename RecordType::address read_address;
-                read_address = static_cast<size_t>(data_file.tellg());
-                if (!data_file.read(reinterpret_cast<char *>(read_id),
-                                    sizeof(RecordType::id))) {
-                    break;
-                }
-                data_page.records[data_page.n_records] =
-                    make_pair(read_id, read_address);
-                data_file.seekg(static_cast<size_t>(data_file.tellg()) +
-                                sizeof(RecordType) - sizeof(RecordType::id));
+        while (
+            data_file.read(reinterpret_cast<char *>(&record), sizeof(record))) {
+            if (data_page.n_records == N<RecordType>) {
+                index_file.write(data_page.c_str(), sizeof(data_page));
+                data_page.n_records = 0;
             }
+            data_page.records[data_page.n_records++] = record;
+        }
+        if (data_page.n_records > 0) {
             index_file.write(data_page.c_str(), sizeof(data_page));
         }
-        index_file.close();
-        data_file.close();
     }
 
+    // Ckecks for the existence of the files that store the indexes
     bool all_indexes_exist() {
         for (int i = 0; i < 3; i++) {
             if (!filesystem::exists(index_paths[i])) {
