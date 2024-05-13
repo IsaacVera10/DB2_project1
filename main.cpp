a42914d52289923d5067563867233f2ae9d08efa
Isaac-VR <isaac.vera@utec.edu.pe>
2024-05-13 02:31:36 -0500

    add: function data_csv to data_bin

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <limits>
 
 //Variables globales constantes
-const int64_t MAX_RECORDS = 913035;
+const int64_t MAX_RECORDS = 913041;
 
 vector<Record_SFile> vec_data;
 void using_vector_records(){
@@ -16,22 +16,25 @@ void using_vector_records(){
 }
 
 int main(){
-    //Generando los registros de .csv a .bin
-    records_csv_to_bin("dataset/movie_dataset.csv", MAX_RECORDS);
+    //1. Descomentar y generar los registros de .csv a .bin
+    // records_csv_to_bin("dataset/movie_dataset.csv"); //Comentar luego de generar toda la data en .bin
 
+    //2. Descomentar para usar leer los registros de .bin
+    // ++++++++++Manejo de movie_dataset.bin++++++++++++
     Sequential_File file("data_sf.bin",1);
 
-    ifstream movie_bin("./dataset/movie_dataset.bin", ios::binary);
-    if(!movie_bin.is_open()) throw runtime_error("Error al abrir el archivo");
+    // ifstream movie_bin("./dataset/movie_dataset.bin", ios::binary);
+    // if(!movie_bin.is_open()) throw runtime_error("Error al abrir el archivo");
 
     // Record_SFile record;
     // while(movie_bin.read(reinterpret_cast<char*>(&record), sizeof(Record_SFile))){
     //     record.showData_line();cout<<endl;
     // //     file.add(record);
     // }
-    movie_bin.seekg(0, ios::end);
-    cout<<"\nCantidad de registros: "<<movie_bin.tellg()/sizeof(Record_SFile)<<endl;
-    movie_bin.close();
+    // movie_bin.seekg(0, ios::end);
+    // cout<<"\nCantidad de registros: "<<movie_bin.tellg()/sizeof(Record_SFile)<<endl;
+    // movie_bin.close();
+    // +++++++++++++++++++++++++++++++++++++++++++++++++
 
     file.print_file("data_sf.bin");
     cout<<endl;
