728154b8f8c09e5f2a56d2893867fe8e7727529e
Isaac-VR <isaac.vera@utec.edu.pe>
2024-05-13 02:33:29 -0500

    add: function data_csv to data_bin

--- main.cpp
+++ main.cpp
@@ -19,23 +19,27 @@ int main(){
     //1. Descomentar y generar los registros de .csv a .bin
     // records_csv_to_bin("dataset/movie_dataset.csv"); //Comentar luego de generar toda la data en .bin
 
-    //2. Descomentar para usar leer los registros de .bin
-    // ++++++++++Manejo de movie_dataset.bin++++++++++++
+
     Sequential_File file("data_sf.bin",1);
 
+    //2. Descomentar para usar leer los registros de .bin
+    // ++++++++++Manejo de movie_dataset.bin++++++++++++
     // ifstream movie_bin("./dataset/movie_dataset.bin", ios::binary);
     // if(!movie_bin.is_open()) throw runtime_error("Error al abrir el archivo");
 
     // Record_SFile record;
-    // while(movie_bin.read(reinterpret_cast<char*>(&record), sizeof(Record_SFile))){
-    //     record.showData_line();cout<<endl;
-    // //     file.add(record);
+    // for(int i = 0; i<MAX_RECORDS; i++){
+    //     movie_bin.read((char*)&record, sizeof(Record_SFile));
+    //     file.add(record);
+    //     // record.showData_line();
     // }
     // movie_bin.seekg(0, ios::end);
     // cout<<"\nCantidad de registros: "<<movie_bin.tellg()/sizeof(Record_SFile)<<endl;
     // movie_bin.close();
     // +++++++++++++++++++++++++++++++++++++++++++++++++
 
+
+    //3. Visualización de archivos
     file.print_file("data_sf.bin");
     cout<<endl;
     file.print_file("aux_sf.bin");
