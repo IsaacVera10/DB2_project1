3a3dfcbbaa7c6c97cbe01fc7e43b1ec42eef9cb5
leonardocandio <leonardo.candio@utec.edu.pe>
2024-05-18 03:13:43 -0500

    built ISAM. It needs a csv file, not binary

--- dataset/data.h
+++ dataset/data.h
@@ -1,10 +1,11 @@
 #ifndef PROYECTO_1_DATA_H
 #define PROYECTO_1_DATA_H
 
-#include <iostream>
-# include "csv.hpp"
+#include "csv.hpp"
 #include <cstring>
 #include <iomanip>
+#include <iostream>
+
 using namespace csv;
 
 using namespace std;
@@ -14,8 +15,7 @@ extern string data_path;
 extern string bin_path;
 //-------- Global Path --------
 
-
-struct Record{
+struct Record {
     int64_t id{};
     char name[71]{};
     float punt_promedio{};
@@ -25,12 +25,13 @@ struct Record{
     int64_t tiempo{};
     char lang[3]{};
 
-    int64_t key_value(){
-        return id;
-    }
+    int64_t key_value() { return id; }
 
     Record() = default;
-    Record(int64_t id, const char* name, float punt_promedio, int64_t vote_count, const char* release_date, int64_t ganancia, int64_t tiempo, const char* lang){
+
+    Record(int64_t id, const char *name, float punt_promedio,
+           int64_t vote_count, const char *release_date, int64_t ganancia,
+           int64_t tiempo, const char *lang) {
         this->id = id;
         strcpy(this->name, name);
         this->punt_promedio = punt_promedio;
@@ -41,19 +42,65 @@ struct Record{
         strcpy(this->lang, lang);
     }
 
-    virtual void showData_line(){
-        cout << setw(8) << left << id<< " | "
-             << setw(74) << left << name<< " | "
-             << setw(6) << left << punt_promedio<< " | "
-             << setw(6) << left << vote_count<< " | "
-             << setw(11) << left << release_date<< " | "
-             << setw(11) << left << ganancia<< " | "
-             << setw(6) << left << tiempo<< " | "
-             << setw(3) << left << lang << endl;
+    explicit Record(const string &line) {
+        string temp;
+        bool inside_quotes = false;
+        int current = 0;
+        for (auto c: line) {
+            if (c == '\"') {
+                inside_quotes = !inside_quotes;
+            } else if (inside_quotes) {
+                temp += c;
+            } else if (c == ',') {
+                switch (current) {
+                    case 0:
+                        id = stoll(temp);
+                        break;
+                    case 1:
+                        strcpy(name, temp.c_str());
+                        break;
+                    case 2:
+                        punt_promedio = stof(temp);
+                        break;
+                    case 3:
+                        vote_count = stoll(temp);
+                        break;
+                    case 4:
+                        strcpy(release_date, temp.c_str());
+                        break;
+                    case 5:
+                        ganancia = stoll(temp);
+                        break;
+                    case 6:
+                        tiempo = stoll(temp);
+                        break;
+                    case 7:
+                        strcpy(lang, temp.c_str());
+                        break;
+                    default:
+                        break;
+                }
+                temp = "";
+                current++;
+            } else {
+                temp += c;
+            }
+        }
+
+
+    }
+
+
+    virtual void showData_line() {
+        cout << setw(8) << left << id << " | " << setw(74) << left << name
+             << " | " << setw(6) << left << punt_promedio << " | " << setw(6)
+             << left << vote_count << " | " << setw(11) << left << release_date
+             << " | " << setw(11) << left << ganancia << " | " << setw(6)
+             << left << tiempo << " | " << setw(3) << left << lang << endl;
     }
 };
 
-void records_csv_to_bin(const string& name_file, int64_t count = -1){
+void records_csv_to_bin(const string &name_file, int64_t count = -1) {
     Record record;
     try {
         csv::CSVFormat format;
@@ -61,29 +108,35 @@ void records_csv_to_bin(const string& name_file, int64_t count = -1){
         format.quote('"');
         format.header_row(0);
 
-        csv::CSVReader reader(data_path+name_file, format);
+        csv::CSVReader reader(data_path + name_file, format);
 
-        fstream file(data_path+"movie_dataset.bin", ios::binary | ios::out | ios::trunc);
-        if(!file.is_open()) throw runtime_error("Error al abrir el archivo");
-        for(auto& row : reader){
-            if(count>0) count --;
-            else if(count == 0) break;
-            
+        fstream file(data_path + "movie_dataset.bin",
+                     ios::binary | ios::out | ios::trunc);
+        if (!file.is_open())
+            throw runtime_error("Error al abrir el archivo");
+        for (auto &row: reader) {
+
+            if (count > 0)
+                count--;
+            else if (count == 0)
+                break;
+
             record.id = row["id"].get<int64_t>();
             strcpy(record.name, row["title"].get<string>().c_str());
             record.punt_promedio = row["vote_average"].get<float>();
             record.vote_count = row["vote_count"].get<int64_t>();
-            strcpy(record.release_date, row["release_date"].get<string>().c_str());
+            strcpy(record.release_date,
+                   row["release_date"].get<string>().c_str());
             record.ganancia = row["revenue"].get<int64_t>();
             record.tiempo = row["runtime"].get<int64_t>();
             strcpy(record.lang, row["original_language"].get<string>().c_str());
-            file.write(reinterpret_cast<char*>(&record), sizeof(Record));
+            file.write(reinterpret_cast<char *>(&record), sizeof(Record));
         }
         file.close();
+    } catch (exception &e) {
+        cerr << "\nError: " << e.what() << endl;
     }
-    catch(exception& e){
-        cerr<<"\nError: "<<e.what()<<endl;
-    }
-
 }
-#endif //PROYECTO_1_DATA_H
+
+
+#endif // PROYECTO_1_DATA_H
--- indexes/ISAM.h
+++ indexes/ISAM.h
@@ -4,6 +4,7 @@
 
 #ifndef DB2_PROJECT1_ISAM_H
 #define DB2_PROJECT1_ISAM_H
+
 #include "iostream"
 #include <cstddef>
 #include <cstring>
@@ -16,84 +17,117 @@
 
 using namespace std;
 
-struct Record_ISAM {
-    long id;
-    char name[71];
-    float punt_promedio;
-    long vote_count;
-    char release_date[11];
-    long ganancia;
-    long tiempo;
-    char lang[3];
-
-    Record_ISAM() = default;
-    Record_ISAM(long _id, const char *_name, float _punt_promedio,
-                long _vote_count, const char *_release_date, long _ganancia,
-                long _tiempo, const char *_lang)
-        : id(_id), punt_promedio(_punt_promedio), vote_count(_vote_count),
-          ganancia(_ganancia), tiempo(_tiempo) {
-        strcpy(name, _name);
-        strcpy(release_date, _release_date);
-        strcpy(lang, _lang);
-    }
-    void showData_line() const {
-        cout << setw(8) << left << id << " | " << setw(74) << left << name
-             << " | " << setw(6) << left << punt_promedio << " | " << setw(6)
-             << left << vote_count << " | " << setw(11) << left << release_date
-             << " | " << setw(11) << left << ganancia << " | " << setw(6)
-             << left << tiempo << " | " << setw(3) << left << lang << " | ";
-    }
-};
-
 ////////////// Index Page ////////////////////
-const int PAGE_SIZE = 512; // Page size for the index
+const int INDEX_PAGE_SIZE = 512; // Page size for the index
 
-template <typename KeyType>
-static constexpr size_t M = (PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) /
+template<typename KeyType>
+static constexpr size_t M = (INDEX_PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) /
                             (sizeof(size_t) + sizeof(KeyType));
 
-template <typename KeyType> struct IndexPage {
+template<typename KeyType>
+struct IndexPage {
     KeyType key[M<KeyType>];           // Array of keys
     size_t children[M<KeyType> + 1]{}; // Pointers to children
     size_t n_keys = 0; // Counts the number of keys in the index page.
 
     char *c_str() { return reinterpret_cast<char *>(this); }
+
+    char *deserialize() { return reinterpret_cast<char *>(this); }
 };
 
 /////////// Data Page ////////
 
-template <typename RecordType>
+const int DATA_PAGE_SIZE = 4096; // Page size for the data
+template<typename RecordType>
 static constexpr size_t N =
-    (PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) / sizeof(RecordType);
-template <typename RecordType> struct DataPage {
+        (DATA_PAGE_SIZE - sizeof(size_t) - sizeof(size_t)) / sizeof(RecordType);
+
+template<typename RecordType>
+struct DataPage {
     RecordType records[N<RecordType>]; // Array of records
     size_t next = -1;                  // Pointer to overflow page
     size_t n_records = 0;              // Number of records
 
     char *c_str() { return reinterpret_cast<char *>(this); }
+
+    char *deserialize() { return reinterpret_cast<char *>(this); }
 };
 
-template <typename RecordType> class ISAM {
-  public:
-    ISAM(filesystem::path _data_path) : data_path(_data_path) { initialize(); }
+template<typename RecordType>
+class ISAM {
+public:
+    ISAM(filesystem::path _csv_path) : csv_path(_csv_path) { initialize(); }
 
-  private:
+    bool add(RecordType new_record) {}
+
+    RecordType search(decltype(RecordType::id) search_id) {
+
+    }
+
+private:
+    filesystem::path csv_path;
     filesystem::path data_path; // Path to the data file
     unordered_map<int, filesystem::path>
-        index_paths;  // Paths to the index files
+            index_paths;  // Paths to the index files
     size_t data_size; // Size of the data file in bytes
+
+    void build_data_file() {
+        vector<RecordType> records;
+        fstream sorted_data_file(data_path, ios::out | ios::binary | ios::trunc);
+        fstream csv_file(csv_path, ios::in);
+
+        if (!csv_file.is_open()) {
+            throw ios_base::failure("Error opening file");
+        }
+
+        string line;
+        getline(csv_file, line); // Skip header
+        while (getline(csv_file, line)) {
+            RecordType record(line);
+            records.push_back(record);
+        }
+
+
+        std::sort(records.begin(), records.end(), [](const RecordType &a, const RecordType &b) {
+            return a.id < b.id;
+        });
+
+        if (!sorted_data_file.is_open()) {
+            throw ios_base::failure("Error opening file");
+        }
+
+        sorted_data_file.write(reinterpret_cast<char *>(records.data()), records.size() * sizeof(RecordType));
+
+        sorted_data_file.close();
+    }
 
     void initialize() {
 
+        if (!filesystem::exists(csv_path)) {
+            throw ios_base::failure("File does not exist");
+        }
+
+        string parent_path;
+        if (csv_path.has_parent_path()) {
+            parent_path = csv_path.parent_path().string() + "/";
+        }
+        data_path = filesystem::path(parent_path + "sorted_" +
+                                     csv_path.stem().string() + ".bin");
+
+        parent_path = "";
+
+        if (!filesystem::exists(data_path)) {
+            build_data_file(); //build sorted data file from csv
+        }
         fstream data_file(data_path, ios::in | ios::binary);
         for (int i = 0; i < 3; i++) {
-            string parent_path;
+
             if (data_path.has_parent_path()) {
-                parent_path = data_path.parent_path().string();
+                parent_path = data_path.parent_path().string() + "/";
             }
             index_paths[i] =
-                filesystem::path(parent_path + "index_" + to_string(i) + "_" +
-                                 data_path.filename().string());
+                    filesystem::path(parent_path + "index_" + to_string(i) + "_" +
+                                     data_path.filename().string());
         }
 
         if (!data_file) {
@@ -131,60 +165,64 @@ template <typename RecordType> class ISAM {
         fstream data_index_file(index_paths[2], ios::in | ios::binary);
         fstream second_index_file(index_paths[1], ios::out | ios::binary);
         DataPage<RecordType> data_index_page;
-        IndexPage<typename RecordType::id> second_index_page;
+        IndexPage<decltype(RecordType::id)> second_index_page;
 
         size_t data_index_page_address = 0;
         // Build second index page from the data index page
-        while (data_index_file.read(reinterpret_cast<char *>(data_index_page),
+        while (data_index_file.read(data_index_page.deserialize(),
                                     sizeof(data_index_page))) {
 
-            if (second_index_page.n_keys == M<typename RecordType::id>) {
+            data_index_page_address = data_index_file.tellg();
+            if (second_index_page.n_keys == M<decltype(RecordType::id)>) {
                 second_index_page.children[second_index_page.n_keys] =
-                    data_index_page_address;
+                        data_index_page_address;
                 second_index_file.write(second_index_page.c_str(),
                                         sizeof(second_index_page));
                 second_index_page.n_keys = 0;
             }
             second_index_page.children[second_index_page.n_keys] =
-                data_index_page_address;
+                    data_index_page_address;
             second_index_page.key[second_index_page.n_keys] =
-                data_index_page.records[0].id;
+                    data_index_page.records[0].id;
             second_index_page.n_keys++;
         }
 
         if (second_index_page.n_keys > 0) {
             second_index_page.children[second_index_page.n_keys] =
-                data_index_page_address;
+                    data_index_page_address;
             second_index_file.write(second_index_page.c_str(),
                                     sizeof(second_index_page));
         }
 
         data_index_file.close();
+        second_index_file.close();
 
+        second_index_file.open(index_paths[1], ios::in | ios::binary);
         // Builds uppermost index page (root) from the second index page
         fstream first_index_file(index_paths[0], ios::out | ios::binary);
-        IndexPage<typename RecordType::id> first_index_page;
+        IndexPage<decltype(RecordType::id)> first_index_page;
         size_t second_index_page_address = 0;
         while (
-            second_index_file.read(reinterpret_cast<char *>(second_index_page),
-                                   sizeof(second_index_page))) {
-            if (first_index_page.n_keys == M<typename RecordType::id>) {
+                second_index_file.read(second_index_page.deserialize(),
+                                       sizeof(second_index_page))) {
+            data_index_page_address = data_index_file.tellg();
+            if (first_index_page.n_keys == M<decltype(RecordType::id)>) {
                 first_index_page.children[first_index_page.n_keys] =
-                    second_index_page_address;
+                        second_index_page_address;
                 first_index_file.write(first_index_page.c_str(),
                                        sizeof(first_index_page));
                 first_index_page.n_keys = 0;
             }
             first_index_page.children[first_index_page.n_keys] =
-                second_index_page_address;
+                    second_index_page_address;
             first_index_page.key[first_index_page.n_keys] =
-                second_index_page.key[0];
+                    second_index_page.key[0];
             first_index_page.n_keys++;
         }
 
         if (first_index_page.n_keys > 0) {
             first_index_page.children[first_index_page.n_keys] =
-                second_index_page_address;
+                    second_index_page_address;
             first_index_file.write(first_index_page.c_str(),
                                    sizeof(first_index_page));
         }
@@ -202,7 +240,7 @@ template <typename RecordType> class ISAM {
         RecordType record;
 
         while (
-            data_file.read(reinterpret_cast<char *>(&record), sizeof(record))) {
+                data_file.read(reinterpret_cast<char *>(&record), sizeof(record))) {
             if (data_page.n_records == N<RecordType>) {
                 index_file.write(data_page.c_str(), sizeof(data_page));
                 data_page.n_records = 0;
